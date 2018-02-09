6cfadf689e06431f83e5c11fd1a1544d6d2b54a7
shireesha12 <karra.shireesha1220@gmail.com>
2018-02-08 20:53:23 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,181 @@
+/*eslint-env browser*/
+
+//STEP1
+/*var someMonth;
+function theMonth()		// function to return current month
+currentMonth;  		// a constant
+var summerMonth  		// an array of summer months
+myLibraryaFunction() 	// a function*/
+
+//STEP2
+ /* 26; //numeric literal expression
+"shireesha"; //a string literal expression
+ true; //a Boolean literal expression
+ null; //a null literal expression*/
+
+//STEP3
+//two examples of complex/variable expressions.
+/*var anExpression = 3 * (6%2) + 6;
+var aSecondExpression = Math.PI * radius * radius;*/
+
+//STEP4
+/*var firstName;
+var lastName;
+var address;
+var city;
+var state;
+var zipCode;
+var yourage;
+var referralSource;
+var mayWeContactYou;*/
+
+//STEP5
+//FIRST METHOD
+/*var firstName;
+var lastName;
+var address;
+var city;
+var state;
+var zipCode;
+var yourAge;
+var referralSource;
+var mayWeContactYou;
+firstName = "shireesha";
+lastName = "karra";
+address = "10660 dabneydrive";
+city = "san diego";
+state = "CA";
+zipCode = 92126;
+yourAge = 26;
+referralSource = null;
+mayWeContactYou ="yes";*/
+
+//second method 
+/*var firstName = "shireesha";
+var lastName = "karra";
+var address = "10660 dabneydrive";
+var city = "san diego";
+var state = "CA";
+var zipCode = 92126;
+var yourAge = 26;
+var referralSource = null;
+var mayWeContactYou ="yes";*/
+
+//third method
+/*var firstName = "shireesha", lastName = "karra", address = "10660 dabneydrive", city = "san diego", state = "CA", 
+zipCode = 92126, yourAge = 26, referralSource = null, mayWeContactYou ="yes";*/
+
+//STEP6
+/*var var1 = 17 + "years";
+window.console.log(var1);
+
+var var2 = true + "siri";
+window.console.log(var2);
+
+var var3 = 2 + true;
+window.console.log(var3);*/
+
+//STEP7
+/*var fName = "siri";
+window.console.log("first name is " + fName + " " + "lastname is" + " " + lName + ".");
+var lName = "karra";*/
+
+//STEP8
+/*var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I am not sure about the former."';
+window.console.log(someString);*/
+
+//STEP9
+/*var x;
+window.console.log(x);
+
+var y = 200;
+var z = null;
+y = null;
+window.console.log(y);
+window.console.log(z);*/
+
+//STEP10
+/*window.console.log(typeof "John");//Returns string
+window.console.log(typeof 89.9);// Returns number
+window.console.log(typeof true);// Returns Boolean
+window.console.log(typeof new Date());//Returns Object
+window.console.log(typeof myCar);//Returns undefined*/
+
+//STEP11
+//window.alert("Hello" + " Zak Ruvalcaba," + " welcome to the JavaScript class!");
+
+//STEP12
+//var name = "Shireesha";
+//window.alert("Hello " + name +" welcome to the JavaScript class!");
+
+//STEP13
+//var name = "Shireesha";
+//var  course = "JavaScript";
+//window.alert("Hello" + " " + name +" welcome to the " + course + " " + "class!");
+
+//STEP14
+//window.alert("Hello Zak Ruvalcaba.\nWelcome to the JavaScript class!");
+
+//STEP15
+//var name = window.prompt("Enter your name:");
+//var  course = "JavaScript";
+//window.alert("Hello" + " " + name +" welcome to the " + course + " " + "class!");
+
+//STEP16
+/*var name = window.prompt("Enter your name:");
+var  course = window.prompt("Enter the class they are taking.");
+window.alert("Hello" + " " + name +" welcome to the " + course + " " + "class!");*/
+
+//STEP17
+/*var x = 10;
+var y = 20;
+window.console.log(x+y);*/
+
+//STEP18
+/*var x = 20;
+window.console.log( x += 20);*/
+
+//STEP19
+/*var x = 20;
+window.console.log( x *= 5);*/
+
+//STEP20
+/*var x = 20%3;
+window.console.log( x /= 1);*/
+
+//STEP21
+/*var x = 10;
+var y = 20;
+window.console.log(x == 10 && y == 20);
+
+//STEP22
+var x = 5;
+var y = 10;
+window.console.log(x <= y != true);
+
+//STEP23
+/*var widget = new Object();
+window.console.log(typeof widget);*/
+
+//STEP24
+var widget = new Object();
+window.console.log(widget instanceof Object);
+
+//STEP25
+var widget = new Object();
+window.console.log(widget instanceof String);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
